1105f66f15a395acb9b32a7fdfca6c3748a03e1b
Vineet Midha <vineet.midha@yahoo.com>
2020-12-04 12:41:17 +0530

    Create QuickSort.cpp

---
+++ QuickSort.cpp
@@ -0,0 +1,22 @@
+// https://practice.geeksforgeeks.org/problems/quick-sort/0/?track=dsa-workshop-1-search-sort&batchId=308
+
+int partition (int a[], int low, int high)
+{
+    int pivot = a[high];
+    
+    int i = low-1;
+    
+    for (int j = low; j <= high-1; j++)
+    {
+        if (a[j] < pivot)
+        {
+            i++;
+            swap (a[i], a[j]);
+        }
+    }
+    
+    swap (a[i + 1], a[high]);
+    
+    return (i + 1);
+}
+
