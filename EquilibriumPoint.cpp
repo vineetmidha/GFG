1c4e8d1ea9063c0341952db86b0d2c73e231fd5c
Vineet Midha <vineet.midha@yahoo.com>
2020-12-09 00:58:03 +0530

    Update EquilibriumPoint.cpp

--- EquilibriumPoint.cpp
+++ EquilibriumPoint.cpp
@@ -50,3 +50,31 @@ as elements before it (1+3) =
 elements after it (2+2).
 
 */
+
+// https://leetcode.com/problems/find-pivot-index/
+
+    int n = a.size();
+
+    if (n==0) return -1;
+
+    if (n==1) return 0;
+
+    int left_sum = 0;
+    int right_sum = 0;
+
+    for (int i=0; i<n; i++){
+        right_sum += a[i];
+    }
+
+    for (int i=0; i<n; i++){
+        right_sum -= a[i];
+
+        if (left_sum == right_sum)
+            return i;
+
+        left_sum += a[i];
+    }        
+
+    return -1;        
+}
+
